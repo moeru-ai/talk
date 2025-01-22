8edf5ae0ac96b9032e4291674da5e99125253d59
藍+85CD <50108258+kwaa@users.noreply.github.com>
2025-01-22 18:39:39 +0800

    chore: remove stylex

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,5 @@
 import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
-import stylex from 'vite-plugin-stylex'
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -11,6 +10,5 @@ export default defineConfig({
         ['babel-plugin-react-compiler', { target: '19' }],
       ] },
     }),
-    stylex(),
   ],
 })
