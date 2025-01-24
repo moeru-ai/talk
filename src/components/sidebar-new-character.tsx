7cb778b300156f82a5edd8d1831bc58a984a0329
藍+85CD <50108258+kwaa@users.noreply.github.com>
2025-01-24 16:43:39 +0800

    feat: drop zone

--- src/components/sidebar-new-character.tsx
+++ src/components/sidebar-new-character.tsx
@@ -1,33 +1,66 @@
 import { Icon } from '@iconify/react'
-import { Button, Dialog, Flex, IconButton, Tooltip } from '@radix-ui/themes'
+import { Button, Dialog, Flex, IconButton, Separator, TextField, Tooltip } from '@radix-ui/themes'
+import { useState } from 'react'
+import { FileTrigger } from 'react-aria-components'
 
-export const SidebarNewCharacter = () => (
-  <Dialog.Root>
-    <Tooltip content="Add a new character" side="right">
-      <Dialog.Trigger>
-        <IconButton size="4" variant="soft">
-          <Icon icon="heroicons:plus" width="20" />
-        </IconButton>
-      </Dialog.Trigger>
-    </Tooltip>
+import { Button as AriaButton } from './aria/button'
+import { DropZone } from './aria/drop-zone'
 
-    <Dialog.Content maxWidth="450px">
-      <Dialog.Title>Add Character</Dialog.Title>
-      {/* <Dialog.Description mb="4" size="2">
-        Make changes to your profile.
-      </Dialog.Description> */}
+export const SidebarNewCharacter = () => {
+  const [files, setFiles] = useState<string>('')
 
-      <Flex direction="column" gap="3">
-        <Button>Upload File (WIP)</Button>
-      </Flex>
+  return (
+    <Dialog.Root>
+      <Tooltip content="Add a new character" side="right">
+        <Dialog.Trigger>
+          <IconButton size="4" variant="soft">
+            <Icon icon="heroicons:plus" width="20" />
+          </IconButton>
+        </Dialog.Trigger>
+      </Tooltip>
 
-      <Flex gap="3" justify="end" mt="4">
-        <Dialog.Close>
-          <Button color="gray" variant="soft">
-            Cancel
+      <Dialog.Content maxWidth="450px">
+        <Dialog.Title>Add Character</Dialog.Title>
+        <Dialog.Description mb="4" size="2">
+          lorem ipsum
+        </Dialog.Description>
+
+        <Flex direction="column" gap="3">
+          <DropZone>
+            <FileTrigger
+              acceptedFileTypes={['application/json', 'image/png']}
+              onSelect={(e) => {
+                if (!e)
+                  return
+
+                setFiles(e[0].name)
+              }}
+            >
+              <AriaButton>
+                <Icon icon="heroicons:document-plus" inline />
+                Select file
+              </AriaButton>
+            </FileTrigger>
+            {files}
+          </DropZone>
+
+          <Separator my="3" size="3" />
+
+          <TextField.Root disabled placeholder="f3f2e850-b5d4-11ef-ac7e-96584d5248b2" />
+          <Button disabled>
+            <Icon icon="heroicons:arrow-down-tray" inline />
+            Download from RisuAI Realms
           </Button>
-        </Dialog.Close>
-      </Flex>
-    </Dialog.Content>
-  </Dialog.Root>
-)
+        </Flex>
+
+        <Flex gap="3" justify="end" mt="4">
+          <Dialog.Close>
+            <Button color="gray" variant="soft">
+              Cancel
+            </Button>
+          </Dialog.Close>
+        </Flex>
+      </Dialog.Content>
+    </Dialog.Root>
+  )
+}
