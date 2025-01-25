e83eaa3edbf1b89aea75e9b274b8be8b09b552d7
藍+85CD <50108258+kwaa@users.noreply.github.com>
2025-01-26 00:56:30 +0800

    refactor(db/schema): add type

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,12 +1,14 @@
 import { sqliteTable, text } from 'drizzle-orm/sqlite-core'
 
+import type { Data } from '../utils/ccv3/types'
+
 export const charactersTable = sqliteTable('characters_table', {
+  // png blob
+  avatar: text({ mode: 'json' }),
+  // character card data json
+  data: text({ mode: 'json' }).$type<Data>().notNull(),
   // uuid v7
   id: text().primaryKey(),
   // character card name
   name: text().notNull(),
-  // png blob
-  avatar: text({ mode: 'json' }),
-  // character card data json
-  data: text({ mode: 'json' }).notNull()
 })
