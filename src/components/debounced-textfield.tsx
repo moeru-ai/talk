5b413c8f350c75f82fe5979ce9590c66e812f99d
iseki <admin@iseki.space>
2025-01-30 03:12:06 +0800

    feat: add a debounced-textfield componet

--- src/components/debounced-textfield.tsx
+++ src/components/debounced-textfield.tsx
@@ -3,13 +3,23 @@ import type { ChangeEventHandler, FC, FocusEventHandler, ReactNode } from 'react
 import { TextField } from '@radix-ui/themes'
 import { useCallback, useState } from 'react'
 
-function withOnBlurValue<P extends WithOnBlurValueRequiredProps>(Component: FC<P>): FC<P & WithOnBlurValueProps>{
-  return (props) => {
-    const { onBlur, onBlurValueChange, onChange, value, ...rest } = props
+interface WithOnBlurValueProps {
+  onBlurValueChange?: (text: string) => undefined | void
+}
+
+interface WithOnBlurValueRequiredProps {
+  children?: ReactNode
+  onBlur?: FocusEventHandler<HTMLInputElement>
+  onChange?: ChangeEventHandler<HTMLInputElement>
+  value?: number | string
+}
+
+function withOnBlurValue<P extends WithOnBlurValueRequiredProps>(Component: FC<P>): FC<P & WithOnBlurValueProps> {
+  return ({ onBlur, onBlurValueChange, onChange, value, ...rest }) => {
     const [indeterminate, setIndeterminate] = useState<typeof value>(undefined)
-    const [prevValue, setPrevValue] = useState(props.value)
-    if (prevValue !== props.value) {
-      setPrevValue(props.value)
+    const [prevValue, setPrevValue] = useState(value)
+    if (prevValue !== value) {
+      setPrevValue(value)
       setIndeterminate(undefined)
     }
     const _onBlur = useCallback<Exclude<typeof onBlur, undefined>>((e) => {
@@ -26,17 +36,6 @@ function withOnBlurValue<P extends WithOnBlurValueRequiredProps>(Component: FC<P
     const p = { onBlur: _onBlur, onChange: _onChange, value: indeterminate ?? value, ...rest }
     return <Component {...p as P} />
   }
-}
-
-interface WithOnBlurValueProps {
-  onBlurValueChange?: (text: string) => undefined | void
-}
-
-interface WithOnBlurValueRequiredProps {
-  children?: ReactNode
-  onBlur?: FocusEventHandler<HTMLInputElement>
-  onChange?: ChangeEventHandler<HTMLInputElement>
-  value?: string | number
 }
 
 export const DebouncedTextField = withOnBlurValue(TextField.Root)
