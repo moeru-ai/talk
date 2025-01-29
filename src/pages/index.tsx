db7f1ceaa920290a47b49b69a3736a26e4877e59
藍+85CD <50108258+kwaa@users.noreply.github.com>
2025-01-29 22:30:12 +0800

    fix(index): clean messages

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,23 @@
 import { Flex } from '@radix-ui/themes'
+import { useEffect } from 'react'
 
 import { Header } from '../components/header'
 import { InputArea } from '../components/input-area'
 import { Messages } from '../components/messages'
+import { useSetMessages } from '../context/messages'
 
-const Index = () => (
-  <Flex direction="column" gap="2" height="100vh" p="2" width="100%">
-    <Header />
-    <Messages />
-    <InputArea />
-  </Flex>
-)
+const Index = () => {
+  const setMessages = useSetMessages()
+
+  useEffect(() => setMessages([]), [setMessages])
+
+  return (
+    <Flex direction="column" gap="2" height="100vh" p="2" width="100%">
+      <Header />
+      <Messages />
+      <InputArea />
+    </Flex>
+  )
+}
 
 export default Index
