3ec086c32d879d746ae22e77b253dec9fa975f3f
藍+85CD <50108258+kwaa@users.noreply.github.com>
2025-01-30 14:51:06 +0800

    refactor: use swr

--- src/pages/room/[uuid]/index.tsx
+++ src/pages/room/[uuid]/index.tsx
@@ -1,35 +1,18 @@
 import { Flex } from '@radix-ui/themes'
-import { eq } from 'drizzle-orm'
-import { useEffect, useState } from 'react'
+import { useEffect } from 'react'
 
 import { Header } from '../../../components/header'
 import { InputArea } from '../../../components/input-area'
 import { Messages } from '../../../components/messages'
 import { useSetMessages } from '../../../context/messages'
-import { db } from '../../../db'
-import { charactersTable } from '../../../db/schema'
+import { useCharacter } from '../../../hooks/use-character'
 import { useParams } from '../../../router'
 import { loadCharacterCard } from '../../../utils/ccv3/load'
 
 const Room = () => {
   const { uuid } = useParams('/room/:uuid')
-  const [character, setCharacter] = useState<typeof charactersTable.$inferSelect | undefined>()
   const setMessages = useSetMessages()
-
-  useEffect(() => {
-    const getCharacter = async () => {
-      const character = await db
-        .select()
-        .from(charactersTable)
-        .where(eq(charactersTable.id, uuid))
-        .get()
-
-      if (character)
-        setCharacter(character)
-    }
-
-    void getCharacter()
-  }, [uuid])
+  const { character } = useCharacter(uuid)
 
   useEffect(() => {
     if (character)
