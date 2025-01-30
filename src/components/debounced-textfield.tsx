123b287ffb8b9a992349c0ae64cd315f8793b43f
iseki <admin@iseki.space>
2025-01-30 10:32:14 +0800

    feat: add a debounced-textfield componet

--- src/components/debounced-textfield.tsx
+++ src/components/debounced-textfield.tsx
@@ -14,28 +14,32 @@ interface WithOnBlurValueRequiredProps {
   value?: number | string
 }
 
-function withOnBlurValue<P extends WithOnBlurValueRequiredProps>(Component: FC<P>): FC<P & WithOnBlurValueProps> {
-  return ({ onBlur, onBlurValueChange, onChange, value, ...rest }) => {
-    const [indeterminate, setIndeterminate] = useState<typeof value>(undefined)
-    const [prevValue, setPrevValue] = useState(value)
-    if (prevValue !== value) {
-      setPrevValue(value)
+const withOnBlurValue = <P extends WithOnBlurValueRequiredProps>(Component: FC<P>): FC<P & WithOnBlurValueProps> => ({
+  onBlur,
+  onBlurValueChange,
+  onChange,
+  value,
+  ...rest
+}) => {
+  const [indeterminate, setIndeterminate] = useState<typeof value>(undefined)
+  const [prevValue, setPrevValue] = useState(value)
+  if (prevValue !== value) {
+    setPrevValue(value)
+    setIndeterminate(undefined)
+  }
+  const _onBlur = useCallback<Exclude<typeof onBlur, undefined>>((e) => {
+    onBlur?.(e)
+    if (indeterminate !== undefined) {
+      onBlurValueChange?.(indeterminate.toString())
       setIndeterminate(undefined)
     }
-    const _onBlur = useCallback<Exclude<typeof onBlur, undefined>>((e) => {
-      onBlur?.(e)
-      if (indeterminate !== undefined) {
-        onBlurValueChange?.(indeterminate.toString())
-        setIndeterminate(undefined)
-      }
-    }, [onBlur, onBlurValueChange, indeterminate, setIndeterminate])
-    const _onChange = useCallback<Exclude<typeof onChange, undefined>>((e) => {
-      onChange?.(e)
-      setIndeterminate(e.currentTarget.value)
-    }, [onChange, setIndeterminate])
-    const p = { onBlur: _onBlur, onChange: _onChange, value: indeterminate ?? value, ...rest }
-    return <Component {...p as P} />
-  }
+  }, [onBlur, onBlurValueChange, indeterminate, setIndeterminate])
+  const _onChange = useCallback<Exclude<typeof onChange, undefined>>((e) => {
+    onChange?.(e)
+    setIndeterminate(e.currentTarget.value)
+  }, [onChange, setIndeterminate])
+  const p = { onBlur: _onBlur, onChange: _onChange, value: indeterminate ?? value, ...rest }
+  return <Component {...p as P} />
 }
 
 export const DebouncedTextField = withOnBlurValue(TextField.Root)
