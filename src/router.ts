bc428ea8b1978bcc40fd535683ab7047b884d515
藍+85CD <50108258+kwaa@users.noreply.github.com>
2025-01-31 15:49:08 +0800

    fix(router): fix lint errors

--- src/router.ts
+++ src/router.ts
@@ -1,17 +1,17 @@
 // Generouted, changes to this file will be overridden
-/* eslint-disable */
 
 import { components, hooks, utils } from '@generouted/react-router/client'
 
-export type Path =
-  | `/`
-  | `/room/:uuid`
+// eslint-disable-next-line sonarjs/redundant-type-aliases
+export type ModalPath = never
 
-export type Params = {
+export interface Params {
   '/room/:uuid': { uuid: string }
 }
 
-export type ModalPath = never
+export type Path =
+  | '/'
+  | '/room/:uuid'
 
 export const { Link, Navigate } = components<Path, Params>()
 export const { useModals, useNavigate, useParams } = hooks<Path, Params, ModalPath>()
