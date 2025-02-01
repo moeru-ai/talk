7d2bf303533c57dbfdb5f89dce3179584fabfd35
藍+85CD <50108258+kwaa@users.noreply.github.com>
2025-02-01 19:24:45 +0800

    feat: welcome message

--- src/components/content.tsx
+++ src/components/content.tsx
@@ -1,15 +1,22 @@
-import { Blockquote, Code, Kbd, Link, Separator, Strong, Text } from '@radix-ui/themes'
+import { Blockquote, Code, Heading, Kbd, Link, Separator, Strong, Text } from '@radix-ui/themes'
 import Markdown from 'react-markdown'
 import remarkGfm from 'remark-gfm'
 
 export const Content = ({ content }: { content: string }) => (
+  // <Flex asChild direction="column" gap="2">
   <Markdown
     components={{
       a: ({ children, href }) => <Link href={href}>{children}</Link>,
       b: ({ children }) => <Strong>{children}</Strong>,
       blockquote: ({ children }) => <Blockquote>{children}</Blockquote>,
       code: ({ children }) => <Code color="gray">{children}</Code>,
-      hr: () => <Separator />,
+      h1: ({ children }) => <Heading size="6">{children}</Heading>,
+      h2: ({ children }) => <Heading size="5">{children}</Heading>,
+      h3: ({ children }) => <Heading size="4">{children}</Heading>,
+      h4: ({ children }) => <Heading size="3">{children}</Heading>,
+      h5: ({ children }) => <Heading size="2">{children}</Heading>,
+      h6: ({ children }) => <Heading size="1">{children}</Heading>,
+      hr: () => <Separator my="3" size="3" />,
       kbd: ({ children }) => <Kbd>{children}</Kbd>,
       p: ({ children }) => <Text>{children}</Text>,
     }}
@@ -17,4 +24,5 @@ export const Content = ({ content }: { content: string }) => (
   >
     {content}
   </Markdown>
+  // </Flex>
 )
